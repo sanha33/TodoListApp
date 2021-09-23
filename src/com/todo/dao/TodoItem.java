56dbed55375e9c05789e417961f5cead43a5883c
sanha33 <48049882+sanha33@users.noreply.github.com>
2021-09-23 22:26:48 +0900

    Update TodoItem.java
    3week assignment

--- src/com/todo/dao/TodoItem.java
+++ src/com/todo/dao/TodoItem.java
@@ -1,18 +1,28 @@
 package com.todo.dao;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class TodoItem {
     private String title;
     private String desc;
-    private Date current_date;
+    private String current_date;
 
 
     public TodoItem(String title, String desc){
         this.title=title;
         this.desc=desc;
-        this.current_date=new Date();
+        SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd kk:mm:ss");
+        this.current_date=f.format(new Date());
     }
+    
+//
+//    public TodoItem(String title, String desc,String date){
+//        this.title=title;
+//        this.desc=desc;
+//     //   SimpleDateFormat f = new SimpleDateFormat("yyyy/MM/dd kk:mm:ss");
+//        this.current_date=date;
+//    }
     
     public String getTitle() {
         return title;
@@ -30,11 +40,20 @@ public class TodoItem {
         this.desc = desc;
     }
 
-    public Date getCurrent_date() {
+    public String getCurrent_date() {
         return current_date;
     }
 
-    public void setCurrent_date(Date current_date) {
+    public void setCurrent_date(String current_date) {
         this.current_date = current_date;
+    }
+    
+    @Override
+    public String toString() {
+    	return "["+title+"] "+ desc + " - " + current_date;
+    }
+    
+    public String toSaveString() {
+    	return title + "##" + desc + "##" + current_date + "\n";
     }
 }
