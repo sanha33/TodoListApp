d4d7de85d1d9a38b4f91a4a551bd3c50a2789322
mount <mountrive@naver.com>
2021-10-17 22:58:39 +0900

    7th project

--- src/com/todo/menu/Menu.java
+++ src/com/todo/menu/Menu.java
@@ -18,7 +18,8 @@ public class Menu {
         System.out.println("12. sort the list by date ( ls_date )");
         System.out.println("13. Find an items ( find )");
         System.out.println("14. Find an category ( find_cate )");
-        System.out.println("15. exit (Or press escape key to exit)");
+        System.out.println("15. Change category ( find_cate )");
+        System.out.println("16. exit (Or press escape key to exit)");
         //System.out.println("Enter your choice >");
     }
     public static void prompt() {
